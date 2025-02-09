90043436656f5c2ca30f37e0b7c92ac3a8b98344
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-02-10 00:12:00 +0200

    added migrations_and id for serializer

--- car/serializers.py
+++ car/serializers.py
@@ -4,6 +4,7 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from car.models import Car
 from car.serializers import CarSerializer
@@ -6,17 +9,15 @@ from car.serializers import CarSerializer
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return json.dumps(
-        serializer.data,
-        ensure_ascii=False
-    ).encode("utf-8")
+    json = JSONRenderer().render(data=serializer.data)
+    return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    data = json.loads(json.decode("utf-8"))
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream=stream)
     serializer = CarSerializer(data=data)
 
     if serializer.is_valid():
-        return serializer.create(serializer.validated_data)
-    else:
-        raise ValueError("Invalid car data: " + str(serializer.errors))
+        return serializer.save()
+    return serializer.errors
