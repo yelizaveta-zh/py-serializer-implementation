e89c7dea9f980db0d415662b5b6d35251a37c318
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-02-10 00:01:24 +0200

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,28 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(1000)
+        ]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(
+        allow_null=True,
+        required=False
+    )
+
+    def create(self, validated_data: dict) -> Car:
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data: dict) -> Car:
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import json
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return json.dumps(
+        serializer.data,
+        ensure_ascii=False
+    ).encode("utf-8")
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    data = json.loads(json.decode("utf-8"))
+    serializer = CarSerializer(data=data)
+
+    if serializer.is_valid():
+        return serializer.create(serializer.validated_data)
+    else:
+        raise ValueError("Invalid car data: " + str(serializer.errors))
